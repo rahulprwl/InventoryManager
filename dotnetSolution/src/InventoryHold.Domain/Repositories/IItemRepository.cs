9ad593429cec12dfb8351510ac05e19d53059580
rporwal <rahulrashmi.porwal@gmail.com>
2026-08-22 05:55:19 -0400

    Adding the Gett All for ITems

--- dotnetSolution/src/InventoryHold.Domain/Repositories/IItemRepository.cs
+++ dotnetSolution/src/InventoryHold.Domain/Repositories/IItemRepository.cs
@@ -6,6 +6,11 @@ public interface IItemRepository
 {
     Task<Item> AddItemAsync(Item item, CancellationToken cancellationToken = default);
 
+    Task<IReadOnlyList<Item>> GetItemsAsync(
+        int offset,
+        int limit = 100,
+        CancellationToken cancellationToken = default);
+
     Task<Item?> UpdateStockAsync(
         Guid itemUuid,
         int stock,
